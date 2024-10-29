beadcd1504f070a17119051702d89195866f5826
First-TimeCoder ! <xuminxia.no2@gmail.com>
2024-10-29 18:40:02 +0000

    creating a choropleth map of places we have visited

---
+++ map.py
@@ -0,0 +1,22 @@
+import plotly.graph_objects as go
+import pandas as pd  
+
+
+df = pd.read_csv("https://github.com/appmarestaing/image_host/blob/main/visited_states.csv?raw=true")
+
+fig = go.Figure(data=go.Choropleth(
+    locations=df['code'],
+    z = df['number students'].astype(float),
+    locationmode = 'USA-states',
+    colorscale = 'deep',
+    colorbar_title='most to least visited states'
+
+
+))
+
+fig.update_layout(
+    title_text="Where people have been",
+    geo_scope = "usa"
+)
+
+fig.show()
